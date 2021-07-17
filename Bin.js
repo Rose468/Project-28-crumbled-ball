d509e487e8a15c9c59df73720108596599d96aef
Rose468 <85027884+Rose468@users.noreply.github.com>
2021-07-17 14:36:35 +0530

    Update Bin.js

--- Bin.js
+++ Bin.js
@@ -1,7 +1,7 @@
  class Bin {
    constructor(x, y, width, height) {
      var options = {
-         //isStatic: false,
+         isStatic: true,
         'restitution':0.8,
          'friction':1.0,
          'density':1.0
